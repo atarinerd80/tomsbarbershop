ceeb1961cd34e79e4756bc4e9582b5cab5205723
Thomas Slagle <tom.slagle@gmail.com>
2022-05-02 12:39:33 -0400

    Added booking button to header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export class Header extends React.Component {
   render() {
+    const bookingLink = 'https://squareup.com/appointments/book/9hnineu11w65qq/LQ3EXB6ZWBERF/services';
     return (
       <>
         <header className="hero-area th-fullpage">
@@ -9,6 +10,11 @@ export class Header extends React.Component {
             <div className="row">
               <div className="col-md-12">
                 <img className="logo-image" src='/img/logo.png'/>
+              </div>
+            </div>
+            <div classname="row">
+              <div className="col-sm-12 col-md-6 offset-md-3">
+                <a className="btn btn-default btn-main booking-button" href={ bookingLink } role="button">Book An Appointment</a>
               </div>
             </div>
           </div>
